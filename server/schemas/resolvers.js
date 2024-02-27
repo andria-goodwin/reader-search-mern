e17a128b313424b9432dd091425800b0c72ba72a
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2024-02-26 18:32:53 -0600

    added queries and mutations and debugging

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,6 +15,7 @@ const resolvers = {
 
     Mutation: {
         addUser: async (parent, { username, email, password }) => {
+          console.log('Add user mutation')
           const user = await User.create({ username, email, password });
           const token = signToken(user);
           return { token, user };
